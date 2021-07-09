8923700a934d65a2cbcd96998fb0e0dff9a41ffb
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-07-09 17:34:40 -0300

    feat(deezer): complete deezer auth functions
    Finished writing all needed functions to pass trhough all deezer auth flow

--- src/services/deezer.js
+++ src/services/deezer.js
@@ -4,4 +4,8 @@ const deezer = axios.create({
   baseURL: "https://cors-anywhere.herokuapp.com/https://api.deezer.com",
 });
 
+export const deezerAuth = axios.create({
+  baseURL: "https://connect.deezer.com/oauth/access_token.php",
+});
+
 export default deezer;
--- src/utils/deezer.js
+++ src/utils/deezer.js
@@ -1,8 +1,43 @@
-import deezerApi from "../services/deezer";
+import deezerApi, { deezerAuth } from "../services/deezer";
+import secrets from "../secrets";
+
+const { deezerAppId, deezerApiSecret, appBaseUrl, deezerPerms } = secrets;
+
+const responseType = "json";
 
 export async function searchPlaylist(id) {
   const result = await deezerApi.get(`/playlist/${id}`);
   return result;
+}
+
+// sends user to login of used service
+export async function redirectToLogin() {
+  window.location.href = `https://connect.deezer.com/oauth/auth.php?app_id=${deezerAppId}&redirect_uri=${appBaseUrl}&perms=${deezerPerms}`;
+}
+
+// retrieve the code from first part of authentication from url
+export function retrieveCode() {
+  let code = null;
+  const queryString = window.location.search;
+  if (queryString.length > 0) {
+    const urlParams = new URLSearchParams(queryString);
+    code = urlParams.get("code");
+  }
+  return code;
+}
+
+// retrieve token from service passing the code
+export async function requestToken(code) {
+  const params = {
+    app_id: deezerAppId,
+    secret: deezerApiSecret,
+    code,
+    output: responseType,
+  };
+  const {
+    data: { accessToken },
+  } = await deezerAuth.get("", { params });
+  return accessToken;
 }
 
 export default { searchPlaylist };
